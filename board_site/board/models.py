97d70a2226f1d735ab8fa46cc76ec25b1ec6efaf
git-kth <hisidblack@kakao.com>
2022-03-26 23:46:58 +0900

    board db modeling

--- board_site/board/models.py
+++ board_site/board/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Board(models.Model):
+    title = models.CharField(max_length=30)
+    name = models.CharField(max_length=40)
+    contents = models.TextField()
+    create_date = models.DateTimeField()
+    update_date = models.DateTimeField()
+    view = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.title
--- board_site/board_site/settings.py
+++ board_site/board_site/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'board',
+    'board.apps.BoardConfig',
     'index',
     'django.contrib.admin',
     'django.contrib.auth',
