6d3405376d9cc58e44f1f982db251689253a04f4
git-kth <hisidblack@kakao.com>
2022-03-27 02:38:07 +0900

    board function add

--- board_site/board/urls.py
+++ board_site/board/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('', views.index_board, name="index_board"),
+    path('board_write/', views.index_board_write, name="index_board_write"),
+    path('<int:idx>/',views.detail , name = "detail")
 ]
--- board_site/board/views.py
+++ board_site/board/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from board.models import Board
 
-def index(request):
-    return HttpResponse("No Board")
+def index_board(request):
+    board_obj = Board.objects.all()
+    reversed_board_obj = board_obj[::-1]
+    info = {'reversed_board_obj': reversed_board_obj}
+    return render(request, 'board.html', info)
+
+def index_board_write(request):
+    return render(request, 'board_write.html')
+
+def index_board_view(request):
+    return render(request, 'board_view.html')
+
+def detail(request, idx):
+    board_obj = Board.objects.get(id=idx)
+    info = {'board_obj': board_obj}
+    return render(request, 'board_view.html', info)
