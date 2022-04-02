7fad3050ccd3252ba86ed683a8295913648a825e
git-kth <hisidblack@kakao.com>
2022-04-02 16:21:07 +0900

    구조 변경

--- board_site/board/urls.py
+++ board_site/board/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'board'
 urlpatterns = [
-    path('', views.index_board, name="index_board"),
-    path('board_write/', views.index_board_write, name="index_board_write"),
-    path('<int:idx>/',views.detail , name = "detail")
+    path('', views.index, name="index"),
+    path('board_write/', views.write, name="write"),
+    path('<int:idx>/',views.detail , name="detail")
 ]
--- board_site/board/views.py
+++ board_site/board/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render
 from board.models import Board
 
-def index_board(request):
+def index(request):
     board_obj = Board.objects.all()
     reversed_board_obj = board_obj[::-1]
     info = {'reversed_board_obj': reversed_board_obj}
-    return render(request, 'board/board.html', info)
+    return render(request, 'board/index.html', info)
 
-def index_board_write(request):
-    return render(request, 'board/board_write.html')
+def write(request):
+    return render(request, 'board/write.html')
 
-def index_board_view(request):
-    return render(request, 'board/board_view.html')
+def view(request):
+    return render(request, 'board/view.html')
 
 def detail(request, idx):
     board_obj = Board.objects.get(id=idx)
     info = {'board_obj': board_obj}
-    return render(request, 'board/board_view.html', info)
+    return render(request, 'board/view.html', info)
