928780c867fbe46e3e8c6fe01444eee97a62f723
git-kth <hisidblack@kakao.com>
2022-04-02 15:43:27 +0900

    간단 수정

--- board_site/board/views.py
+++ board_site/board/views.py
@@ -5,15 +5,15 @@ def index_board(request):
     board_obj = Board.objects.all()
     reversed_board_obj = board_obj[::-1]
     info = {'reversed_board_obj': reversed_board_obj}
-    return render(request, 'board.html', info)
+    return render(request, 'board/board.html', info)
 
 def index_board_write(request):
-    return render(request, 'board_write.html')
+    return render(request, 'board/board_write.html')
 
 def index_board_view(request):
-    return render(request, 'board_view.html')
+    return render(request, 'board/board_view.html')
 
 def detail(request, idx):
     board_obj = Board.objects.get(id=idx)
     info = {'board_obj': board_obj}
-    return render(request, 'board_view.html', info)
+    return render(request, 'board/board_view.html', info)
--- board_site/board_site/urls.py
+++ board_site/board_site/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', include("index.urls")),
     path('board/', include("board.urls")),
+    path('', RedirectView.as_view(url='index/', permanent=True)),
 ]
