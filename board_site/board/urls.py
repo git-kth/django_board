7abee0fdb9f7a1573c0d7159b1dcf80082ee9a29
git-kth <hisidblack@kakao.com>
2022-06-05 20:51:22 +0900

    리펙터링

--- board_site/board/urls.py
+++ board_site/board/urls.py
@@ -6,19 +6,17 @@ from .views import base_views, board_views, comment_views
 app_name = 'board'
 
 urlpatterns = [
+    # base_views
     path('', base_views.index, name='index'),
     path('<int:board_id>/',base_views.detail , name="detail"),
+    
+    # board_views
     path('board_create/', board_views.board_create, name="board_create"),
     path('board_modify/<int:board_id>', board_views.board_modify, name="board_modify"),
     path('board_delete/<int:board_id>', board_views.board_delete, name="board_delete"),
+    
+    # comment_views
     path('comment_create/<int:board_id>', comment_views.comment_create, name="comment_create"),
     path('comment_modify/<int:comment_id>', comment_views.comment_modify, name="comment_modify"),
     path('comment_delete/<int:comment_id>', comment_views.comment_delete, name="comment_delete"),
-    # path('write/', views.write, name="write"),
-    # path('add/',views.add, name ='add'),
-    # path('delete/<int:idx>/',views.delete, name ="delete"),
-    # path('update/<int:idx>/update_form',views.update_form , name = "update_form"),
-    # path('update/<int:idx>',views.update , name = "update"),
-    # path('page/<int:idx>/', views.paging, name="paging"),
-    # path('comment_reg/<int:idx>/',views.comment_reg, name ="comment_reg"),
 ]
